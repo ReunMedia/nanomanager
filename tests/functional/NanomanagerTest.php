911ed765bd5c204f62611e5238b58f2affc3657c
Kimmo Salmela <kimmo.salmela@reun.eu>
2025-08-27 20:12:34 +0300

    Add initial failing tests

---
+++ tests/functional/NanomanagerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+describe("Nanomanager", function () {
+    it("should display a list of files in the selected folder", function () {})->todo();
+    it("should allow uploading files", function () {})->todo();
+    it("should allow deleting files", function () {})->todo();
+    it("should allow renaming files", function () {})->todo();
+});
