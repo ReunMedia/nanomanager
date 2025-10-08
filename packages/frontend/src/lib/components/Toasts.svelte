3c42704cd3335af578161c98f904316f1223fa55
Kimmo Salmela <kimmo.salmela@reun.eu>
2025-10-08 16:07:35 +0300

    Add background and shadow to toasts

--- packages/frontend/src/lib/components/Toasts.svelte
+++ packages/frontend/src/lib/components/Toasts.svelte
@@ -15,13 +15,21 @@
 <style>
   dialog {
     position: fixed;
-    margin: auto;
+    margin: 0 auto 1em auto;
     bottom: 0;
     border: none;
     background: none;
+    padding: 0;
+    /* Don't block pointer on invisible dialog overlay */
+    pointer-events: none;
   }
 
   li {
     margin-top: 1em;
+    background: var(--color-bg);
+    /* Re-enable pointer events on visible toasts */
+    pointer-events: auto;
+
+    box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 8px 0px;
   }
 </style>
