43d618033119e6d87196cbdc10bbb9027ddc0638
Kimmo Salmela <kimmo.salmela@reun.eu>
2025-10-15 15:27:19 +0300

    Minify CSS with cssnano

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import liveReload from "vite-plugin-live-reload";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
+import cssnano from "cssnano";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), liveReload(["../php/src/**/*.php"]), ViteMinifyPlugin()],
+  css: {
+    postcss: {
+      plugins: [cssnano],
+    },
+  },
 });
