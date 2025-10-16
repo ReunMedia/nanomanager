341d2b65f5a86cf781ecc88264c77d9b0140b6d3
Kimmo Salmela <kimmo.salmela@reun.eu>
2025-10-16 15:16:26 +0300

    Include root dist to git repo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,9 @@
 // @ts-check
 import tseslint from "typescript-eslint";
 import createReunMediaConfig from "@reunmedia/eslint-config";
+import { globalIgnores } from "eslint/config";
 
-export default tseslint.config(await createReunMediaConfig(import.meta.url));
+export default tseslint.config([
+  ...(await createReunMediaConfig(import.meta.url)),
+  globalIgnores(["dist"]),
+]);
